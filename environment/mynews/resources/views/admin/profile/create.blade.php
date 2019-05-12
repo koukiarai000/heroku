932ab7eb4b51b4b48c6ff0d784543d95dea0f016
koukiarai000 <acidcherry373@gmail.com>
2019-05-12 14:14:54 +0000

    mynews

---
+++ environment/mynews/resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+     <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edeg">
+       <mata name="viewport" content="width=device-width",initial-scale="1">
+       <title>Mynews</title>
+     </head>
+     <body>
+       <h1>Myニュース作成画像</h1>
+     </body>
+</html>
