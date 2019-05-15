85cda9d0aac015359ca9e345cbe4494b2ccb95b9
koukiarai000 <acidcherry373@gmail.com>
2019-05-15 15:27:14 +0000

    ユーザ認証

--- environment/mynews/routes/web.php
+++ environment/mynews/routes/web.php
@@ -17,9 +17,14 @@ Route::get('/', function () {
 
 //ルーティング設定のグループ化
 Route::group(['prefix' =>'admin'],function(){
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
 });
 
 
-Route::get('admin/profile/create','Admin\ProfileController@add');
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
+Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
