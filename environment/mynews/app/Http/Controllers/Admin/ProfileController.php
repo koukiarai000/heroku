38a7423df6ff0b19e1768f6eb320d1d705349217
koukiarai000 <acidcherry373@gmail.com>
2019-05-21 09:43:13 +0000

    投稿したニュースの更新・削除

--- environment/mynews/app/Http/Controllers/Admin/ProfileController.php
+++ environment/mynews/app/Http/Controllers/Admin/ProfileController.php
@@ -4,24 +4,79 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\profiles;
 
 class ProfileController extends Controller
 {
     public function add()
     {
-        //create.blade.htmlファイルを呼び出す
+        //resources/views/admin/profile/create.blade.phpファイルを呼び出す
         return view('admin.profile.create');
     }
-    public function create()
+    
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
+        // Varidationを行う
+        $this->validate($request, profiles::$rules);
+        $profiles = new profiles;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+        
+        return redirect('admin/profile');
     }
-    public function edit()
+    
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        //$requestの中のcond_titleを代入している
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            //検索されたら検索結果を取得する
+            $posts = profiles::where('name',$cond_name)->get();
+        } else {
+            //それ以外は全てのニュースを取得する
+            $posts = profiles::all();
+        }
+        return view('admin.profile.index',['posts' => $posts, 'cond_name'=> $cond_name]);
     }
-    public function update()
+    
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
+        //¥app¥profiles Modelからデータを取得する
+        $profiles = profiles::find($request->id);
+        if (empty($profiles)) {
+            abort(404);
+        }
+        return view('admin.profile.edit',['profiles_form' => $profiles]);
     }
+    
+    public function update(Request $request)
+    {
+        //Validationをかける
+        $this->validate($request, profiles::$rules);
+        
+        //profiles Modelからデータを取得する
+        $profiles = profiles::find($request->id);
+        
+        //送信さてきたフォームデータを格納する
+        $profiles_form = $request->all();
+        unset($profiles_form['_token']);
+        
+        //該当するデータを上書きして保存する
+        $profiles->fill($profiles_form)->save();
+        
+        return redirect('admin/profile');
+    }
+    public function delete(Request $request)
+    {
+        $profiles = profiles::find($request->id);
+        $profiles->delete();
+        return redirect('admin/profile/');
+    }
+    
 }
